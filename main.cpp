23014e3c4ccd8ce15f7cec3e40cb58fd3e169f52
Flind4wg <dzakyy.gaming01@gmail.com>
2026-06-26 19:00:14 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
-#include <hook.h>
+#include <jni.h>
 
-TCHAR_HOOK(void, "?applyFog@FogManager@@QEAAXAEAVRenderContext@@AEAVPlayer@@M@Z", 
-           void* renderContext, void* player, float fogDistance) {
-    original(renderContext, player, 999999.0f); 
-}
-
-void entry() {
+extern "C" {
+    void _init(void) {
+    }
 }
